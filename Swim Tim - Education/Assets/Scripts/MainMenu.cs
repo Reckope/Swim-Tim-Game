cc10f3edb70f9566f54a3f42667c268657cabc1d
Joe Davis <joe--davis@hotmail.com>
2018-02-05 16:55:20 +0000

    Main Menu
    Added 'MainMenu' scene

---
+++ Swim Tim - Education/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {	
+
+	public string startGameScene;
+
+	public void StartGame ()
+	{
+		Application.LoadLevel (startGameScene);
+	}
+
+	public void Rules ()
+	{
+		Application.LoadLevel (startGameScene);
+	}
+
+}
+
--- Swim Tim - Education/Assets/Scripts/ObjectSpeed.cs
+++ Swim Tim - Education/Assets/Scripts/ObjectSpeed.cs
@@ -2,16 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScrollingBackground : MonoBehaviour {
+public class ObjectSpeed : MonoBehaviour {
 
 	private Rigidbody2D rb2d;
-	public float objectSpeed;
+	public float speed;
 
 	// Use this for initialization
 	void Start () {
 
 		rb2d = GetComponent<Rigidbody2D> ();
-		rb2d.velocity = new Vector2 (GameControl.instance.scrollSpeed, 0); 
+		rb2d.velocity = new Vector2 (speed, 0);
 		
 	}
 	
