edbbb3fd8a9c49a1b05935afa769c7ca9f3b437e
Joe Davis <joe--davis@hotmail.com>
2018-03-01 21:11:09 +0000

    V 1.1.4
    Designed the multiple 'Rules' pages.
    Still yet to add scripts to cycle through them.

---
+++ Swim Tim - Education/Assets/Scripts/RulesControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RulesControl : MonoBehaviour {
+
+	public GameObject Controls;
+	public GameObject Gameplay;
+	public GameObject Enemies;
+	public GameObject Questions;
+
+
+	// Use this for initialization
+	void Start () {
+
+		Controls.SetActive (true);
+		Gameplay.SetActive (false);
+		Enemies.SetActive (false);
+		Questions.SetActive (false);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
